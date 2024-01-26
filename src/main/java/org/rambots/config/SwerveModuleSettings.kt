e10b5e4699b5642e065382b8d48cd4dbbf79f5fd
pep233 <yibai14651@gmail.com>
2024-01-26 15:44:38 -0800

    Updated CAN ids

--- src/main/java/org/rambots/config/SwerveModuleSettings.kt
+++ src/main/java/org/rambots/config/SwerveModuleSettings.kt
@@ -4,17 +4,17 @@ import edu.wpi.first.math.geometry.Rotation2d
 import org.rambots.lib.swerve.SwerveModuleConfiguration
 
 enum class SwerveModuleSettings(
-    override val driveMotorCANId: Int,
     override val steerMotorCANId: Int,
     override val steerEncoderCANId: Int,
+    override val driveMotorCANId: Int,
     override val steerEncoderOffset: Rotation2d,
     override val simulationEncoderOffset: Rotation2d
 ) : SwerveModuleConfiguration {
     /** values to be updated */
-    FRONT_LEFT(0, 0, 0, 0.0.degrees, 0.0.degrees),
-    FRONT_RIGHT(0, 0, 0, 0.0.degrees, 0.0.degrees),
-    BACK_LEFT(0, 0, 0, 0.0.degrees, 0.0.degrees),
-    BACK_RIGHT(0, 0, 0, 0.0.degrees, 0.0.degrees),
+    FRONT_LEFT(1, 2, 3, 0.0.degrees, 0.0.degrees),
+    FRONT_RIGHT(4, 5, 6, 0.0.degrees, 0.0.degrees),
+    BACK_LEFT(10, 11, 12, 0.0.degrees, 0.0.degrees),
+    BACK_RIGHT(7, 8, 9, 0.0.degrees, 0.0.degrees),
 }
 
 /* Double extension property. Allows to convert a double to a rotation 2d object. */
