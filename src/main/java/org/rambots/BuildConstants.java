5e8a73aaa4496c06ad1a4e91d3936537282b250a
James Ding <jamesding365@gmail.com>
2024-03-02 21:55:19 -0800

    Remove spotless

--- src/main/java/org/rambots/BuildConstants.java
+++ src/main/java/org/rambots/BuildConstants.java
@@ -4,17 +4,16 @@ package org.rambots;
  * Automatically generated file containing build version information.
  */
 public final class BuildConstants {
-    public static final String MAVEN_GROUP = "";
-    public static final String MAVEN_NAME = "frc-2024";
-    public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 1;
-    public static final String GIT_SHA = "b55720df88c29f28dfcd9e76a0256378797c67ac";
-    public static final String GIT_DATE = "2024-01-12 08:28:13 PST";
-    public static final String GIT_BRANCH = "james/swerve";
-    public static final String BUILD_DATE = "2024-03-02 15:32:31 PST";
-    public static final long BUILD_UNIX_TIME = 1709422351326L;
-    public static final int DIRTY = 1;
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "frc-2024";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 4;
+  public static final String GIT_SHA = "08332d209d5099e406b9c6e408f1753c0f948987";
+  public static final String GIT_DATE = "2024-03-02 21:49:43 PST";
+  public static final String GIT_BRANCH = "james/swerve";
+  public static final String BUILD_DATE = "2024-03-02 21:54:52 PST";
+  public static final long BUILD_UNIX_TIME = 1709445292254L;
+  public static final int DIRTY = 1;
 
-    private BuildConstants() {
-    }
+  private BuildConstants(){}
 }
