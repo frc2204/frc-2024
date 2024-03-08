bbfde519a104aa10b9963d4df2e00fc23b3d612a
pep233 <yibai14651@gmail.com>
2024-03-08 09:21:11 -0800

    Superstructure Subsystem

--- src/main/java/org/rambots/subsystems/SuperstructureSubsystem.kt
+++ src/main/java/org/rambots/subsystems/SuperstructureSubsystem.kt
@@ -1,9 +1,22 @@
 package org.rambots.subsystems
 
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
+import edu.wpi.first.wpilibj2.command.WaitCommand
+
 object SuperstructureSubsystem {
-    /** Intake */
     fun intake() {
-        WristSubsystem.wristIntakePosition()
-        ArmSubsystem.armIntakePosition()
+        SequentialCommandGroup(
+            ElevatorSubsystem.elevatorIntakePosition(),
+            ArmSubsystem.armIntakePosition(),
+            WristSubsystem.wristIntakePosition()
+        )
+    }
+
+    fun climb() {
+        SequentialCommandGroup(
+            ElevatorSubsystem.extend(),
+            WaitCommand(1.0),
+            ElevatorSubsystem.retract()
+        ).schedule()
     }
 }
