90b8a2f93ae021d74c1c196d53f6d0ac655ac1fd
pep233 <yibai14651@gmail.com>
2024-04-01 10:26:51 -0700

    AdvantageKit vendordep 3.2.0

---
+++ src/main/java/org/rambots/BuildConstants.java
@@ -0,0 +1,19 @@
+package org.rambots;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "frc-2024.2";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 47;
+  public static final String GIT_SHA = "620c2c3e6ebfb2b9e742b9cd48ae7f2e66e32538";
+  public static final String GIT_DATE = "2024-03-27 17:36:31 PDT";
+  public static final String GIT_BRANCH = "jay/Autos/temp";
+  public static final String BUILD_DATE = "2024-04-01 10:25:27 PDT";
+  public static final long BUILD_UNIX_TIME = 1711992327275L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
